e04a8513aa4278e9ae679ec33c9d1f45e64b66d1
richu101 <richubini101@gmail.com>
2020-12-15 10:08:24 +0000

    drom sound locatation added

--- python-projrcts/pyserial/serialdrum.py
+++ python-projrcts/pyserial/serialdrum.py
@@ -1,6 +1,5 @@
 from pygame import mixer
 import serial
-import time
 
 i = 0.5
 # Starting the mixer
@@ -10,11 +9,11 @@ mixer.init()
 
 frmt = ".mp3"
 def play_music(voice):
-    """
-    This function is to play the drum audios in the drum sound audios
-    """
+    # This function is to play the drum audios in the drum sound audios
+    
     mixer.music.load(voice)
     print(voice)
+    
     # Start playing the song
     mixer.music.play()
 
@@ -22,9 +21,9 @@ ser = serial.Serial('/dev/ttyACM0')  # open serial port
 print(ser.name)
 while 1:
     s = str(ser.readline(1))  
-   # time.sleep(.2)
     frmt = ".mp3"
     direction = "/home/pi/Documents/Python-keynotes-And-projects/python-projrcts/Drum sounds/sound_track/"
+    # The sounds are  included in the drum sound folder 
     snd = direction + s[2:-1] + frmt
     play_music(snd)
     print(snd)
