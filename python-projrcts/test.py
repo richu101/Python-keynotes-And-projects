4ce7635e673212d1b6ee8251d57096f10e15c9ea
richu101 <richubini101@gmail.com>
2021-10-15 22:22:56 +0530

    test added

--- python-projrcts/test.py
+++ python-projrcts/test.py
@@ -1,20 +1,2 @@
-def summ(m):
-    s = 0
-    while(m>1):
-        s = s + m%10
-        m = m/10
-     
-    return int(s)
-
-num = int(input("enter the total number u r going to enter"))
-
-
-sum = 0
-while(num!=0):
-    val = int(input(" "))
-    sum = sum + summ(val) 
-    print(sum)
-    num = num-1
-print("final ",sum)
-
-
+for i in range(10,1,-1):
+    print(i)
