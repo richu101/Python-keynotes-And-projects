5290eb1b918035d03699bdbb6a98e8f894605051
richu101 <richubini101@gmail.com>
2021-10-15 22:17:49 +0530

    some test code

--- python-projrcts/test.py
+++ python-projrcts/test.py
@@ -1,3 +1,20 @@
-str = "How are you"
+def summ(m):
+    s = 0
+    while(m>1):
+        s = s + m%10
+        m = m/10
+     
+    return int(s)
 
-print(str.lstrip("H").lstrip("o"))
+num = int(input("enter the total number u r going to enter"))
+
+
+sum = 0
+while(num!=0):
+    val = int(input(" "))
+    sum = sum + summ(val) 
+    print(sum)
+    num = num-1
+print("final ",sum)
+
+
